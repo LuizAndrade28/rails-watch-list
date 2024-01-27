378a485edc651bda0b5b6f1ae716b04581bbf85e
LuizAndrade <lcfsandrade@gmail.com>
2024-01-26 22:39:09 -0300

    Updated app

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  has_one_attached :photo
 
   validates :comment, length: { minimum: 6 }
   # validates_uniqueness_of :movie, conditions: -> { where.not(status: 'Movie has already been taken') }
